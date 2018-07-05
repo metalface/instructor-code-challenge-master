c76166655991adfaecef5926471f18a7af57ab8c
Jeff Gamble <jef.m.gamble@gmail.com>
2018-07-05 18:51:06 -0400

    Commit message

--- node-backend-starter-code/server.js
+++ node-backend-starter-code/server.js
@@ -1,10 +1,10 @@
 var express = require("express");
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); //Added body-parser require
 var app = express();
 var fs = require("fs");
 var path = require("path");
 
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/public"))); //Closed  parenthesis
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -23,12 +23,12 @@ app.get("favorites", function(req, res){
 
   var data = JSON.parse(fs.readFileSync("./data.json"));
   data.push(req.body);
-  fs.writeFile("./data.json", JSON.stringify(data));
+  fs.writeFile("./data.json", JSON.stringify(data)); //Closed parenthesis
   res.setHeader("Content-Type", "application/json");
   res.send(data);
 }
 
-app.listen(3000, function(){
-  console.log('listening on port 3000!');
+app.listen(3000, function(){ //Corrected to app.listen
+  console.log("listening on port 3000!");
 });
-});
+}); //Closed function
